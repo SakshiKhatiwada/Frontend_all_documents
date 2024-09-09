e560e80eb232092e676565ea2626c516a7ccb416
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2024-09-09 22:52:23 +0545

    Progress bar updated and some files removes

--- 1.Rough/rough.js
+++ 1.Rough/rough.js
@@ -1,12 +1,16 @@
-let input = document.querySelector("input");
-let log = console.log;
-log(input);
+// "use strict";
+// let input = document.querySelector("input");
+// let log = console.log;
+// log(input);
 
-let button = document.querySelector("button");
+// let button = document.querySelector("button");
 
-button.addEventListener("click", function(){
-    let alarmTime = input.value;
-    log(alarmTime);
-    let alarmTimeExtracted = new Date("2024-09-07T10:15");
-    log(alarmTimeExtracted);
-})
+// button.addEventListener("click", function(){
+//     let alarmTime = input.value;
+//     log(alarmTime);
+//     let alarmTimeExtracted = new Date("2024-09-07T10:15");
+//     log(alarmTimeExtracted);
+// })
+
+Name = "sakshi";
+console.log(Name);
