b6f4761deeb660d4042c267ae3dbf22ec1595c2a
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2024-10-08 21:45:31 +0545

    typescript day1

---
+++ 1.Rough/assigningFunc.js
@@ -0,0 +1,26 @@
+const xy= function(){
+    console.log('sakshi');
+  }
+
+//   xy();
+ 
+  const func= ()=>console.log('returned');
+//   func();
+
+//   sakshi();
+
+
+
+  {
+    
+    // let y=2;
+    // let x=y;
+
+    let x=y=2;
+    // console.log(x,y); //2,2
+    
+    // let z = sakshi; //
+    // sakshi();
+    // z();
+    
+  }
--- Daily Assignments/basic topics/AONoperators.js
+++ Daily Assignments/basic topics/AONoperators.js
@@ -10,4 +10,24 @@ console.log(false || "s");
 console.log("haha" || false);
 console.log(1 || 2);    //1
 
-console.log ( x ?? 'n/a');  //prints the second operand if first one is not available | only works with NULL or UNDEFINED
+console.log ( x ?? 'n/a');  //NOTE //prints the second operand if first one is not available | only works with NULL or UNDEFINED
+
+
+//   console.log(null ?? "hi");
+//   console.log(5 ?? "hi");
+//   console.log(NaN ?? "hi");
+//   console.log(undefined ?? "hi");
+
+// let num = 5;
+
+let num = "rs123";
+console.log(typeof num);
+console.log(num);
+
+let num2 = Number(num);
+console.log(num2);
+console.log(typeof num2);
+
+// let num3 = parseInt(num);
+let num3 = parseFloat(num);
+console.log(num3, typeof num3);
