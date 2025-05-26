c013865a59a01735b04b6db9f728313db4ca5d5a
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-05-26 23:14:09 +0545

    tiny recap

--- 5th-Sem-WebTechnology/PHP/index.php
+++ 5th-Sem-WebTechnology/PHP/index.php
@@ -11,13 +11,25 @@
   <!-- <h1>php practice</h1> -->
 
   <?php
+  $hi = "s";
   echo "Hello php! <br/>";
   echo "Hello php!<br/>";
   echo "Hello" . 'PHP' .
     'hihi'; // concatenation
+  print "hi";
+  echo "<br>$hi bye";
+  $muld = array(
+    "breakfast" => ["Walnut", "Roti"],
+    "lunch" => ["pulau", "puri"],
+    "dinner" => ["roti", "vegetable"],
+  );
+  print ($muld);
+  echo "<br>";
+  print_r($muld);
   ?>
   <!-- NOTE php can be written above the html, below, or inside, anywhere -->
   <!-- PHP engine process these and send processed HTML to the browser -->
+
 
 </body>
 
